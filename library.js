f4d51430f10cb05d6afbab46ac1d54c04c0afa57
Richie <richiehopkinson@hotmail.co.uk>
2025-07-23 13:39:59 +0100

    Added code to "Delete This Book" and "Toggle Read" buttons. Added books count. Need to add: Confirmation dialogue for delete and "Add A Book" form and functionality.

--- library.js
+++ library.js
@@ -1,6 +1,14 @@
 // Create library array
 let myLibrary = [];
 let list = document.getElementById("bookList");
+let currentID = "";
+let currentIndex = 0;
+let totalBooks = 0;
+let readText = ""
+const deleteButton = document.getElementById("delete");
+const addButton = document.getElementById("add");
+const toggleReadButton = document.getElementById("toggleRead");
+var booksCount = document.getElementById("booksCount");
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true, "images/The-Hobbit.jpg");
 addBookToLibrary('The Fellowship Of The Ring', 'J.R.R. Tolkien', 423, true, "images/The Fellowship Of The Ring.jpeg");
@@ -18,8 +26,6 @@ addBookToLibrary('Vault: A LitRPG Adventure (Unbound Book 8)', 'Nicoli Gonnella'
 addBookToLibrary('Crown: A LitRPG Adventure (Unbound Book 9)', 'Nicoli Gonnella', 949, true, "images/Crown.jpg");
 addBookToLibrary('Empire: A LitRPG Adventure (Unbound Book 10)', 'Nicoli Gonnella', 752, true, "images/Empire.jpg");
 addBookToLibrary('Chains: A LitRPG Adventure (Unbound Book 11)', 'Nicoli Gonnella', 762, true, "images/Chains.jpg");
-
-console.log(myLibrary[6].author);
 
 // Constructor for Book object
 function Book(title, author, pages, readOrNot, imageURL) {
@@ -40,49 +46,72 @@ function addBookToLibrary(title, author, pages, read, imageURL) {
     myLibrary.push(book);
 }
 
-function displayBooks() {
-    // for (a = 0; a < 15; a++) {
-        for (var i = 0; i < myLibrary.length; i++) {
-            let li = document.createElement("li")
-            let index = i
-            let readText = ""
-            li.innerHTML = myLibrary[i].title
-            li.className = "bookListItems"
-            li.addEventListener("click", () => {
-                var authorName = document.getElementById("selectedBookDetails").rows[parseInt(0,10)].cells
-                var numPages = document.getElementById("selectedBookDetails").rows[parseInt(1,10)].cells
-                var readYet = document.getElementById("selectedBookDetails").rows[parseInt(2,10)].cells                              
-                authorName[parseInt(1,10)].innerHTML = myLibrary[index].author;
-                numPages[parseInt(1,10)].innerHTML = myLibrary[index].pages;
-                if (myLibrary[index].readOrNot === true) {
-                    readText = "Yes"
-                } else {
-                    readText = "No"
-                }
-                readYet[parseInt(1,10)].innerHTML = readText;
-                const image = document.getElementById("bookPicture");
-                image.src = myLibrary[index].imageURL;
-                // Set all backgrounds to transparent.
-                var lists = document.querySelectorAll("li");
-                lists.forEach((li) => li.style.backgroundColor = "transparent");
-                lists.forEach((li) => li.style.color = "black");
-                // Set selected background to grey.
-                li.style.backgroundColor = "grey";
-                li.style.color = "white";
-            })
-            list.appendChild(li);
-        }
-    // }
+function displayBookDetail(index) {
+    currentID = myLibrary[index].id;
+    currentIndex = index;
+    console.log(currentIndex);
+    var authorName = document.getElementById("selectedBookDetails").rows[parseInt(0,10)].cells
+    var numPages = document.getElementById("selectedBookDetails").rows[parseInt(1,10)].cells
+    var readYet = document.getElementById("selectedBookDetails").rows[parseInt(2,10)].cells                             
+    authorName[parseInt(1,10)].innerHTML = myLibrary[index].author;
+    numPages[parseInt(1,10)].innerHTML = myLibrary[index].pages;
+    if (myLibrary[index].readOrNot === true) {
+        readText = "Yes"
+    } else {
+        readText = "No"
+    }
+    readYet[parseInt(1,10)].innerHTML = readText;
+    const image = document.getElementById("bookPicture");
+    image.src = myLibrary[index].imageURL;                
 }
+
+function displayBooks() {
+    totalBooks = 0;
+    for (var i = 0; i < myLibrary.length; i++) {
+        let li = document.createElement("li")
+        let index = i
+        li.innerHTML = myLibrary[i].title
+        li.className = "bookListItems"
+        // Display the image and details of the first book in the list
+        if (i === 0) {
+            displayBookDetail(index)
+        }
+
+        li.addEventListener("click", () => {
+            displayBookDetail(index)
+            var lists = document.querySelectorAll("li");
+            lists.forEach((li) => li.style.backgroundColor = "transparent");
+            lists.forEach((li) => li.style.color = "black");
+            // Set selected background to grey.
+            li.style.backgroundColor = "grey";
+            li.style.color = "white";
+        })
+        list.appendChild(li);
+        totalBooks = ++totalBooks;
+    }
+}
+
+// Toggle the read status
+toggleReadButton.addEventListener("click", () => {
+    var readYet = document.getElementById("selectedBookDetails").rows[parseInt(2,10)].cells   
+    myLibrary[currentIndex].readOrNot = !myLibrary[currentIndex].readOrNot;
+    if (myLibrary[currentIndex].readOrNot === true) {
+        readText = "Yes"
+    } else {
+        readText = "No"
+    }
+    readYet[parseInt(1,10)].innerHTML = readText;    
+})    
 
 // Remove a book from the library
-function deleteBookFromLibrary() {
-    // take params, remove a book from the array
-}
-
-function markBookAsReadOrUnread() {
-    //toggle the read status
-}
+deleteButton.addEventListener("click", () => {
+    myLibrary.splice(currentIndex, 1);
+    var ul = document.getElementById("bookList");
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
+    displayBooks();
+    booksCount.innerHTML = "Total books:" + totalBooks;
+})
 
 displayBooks();
+booksCount.innerHTML = "Total books:" + totalBooks;
 
